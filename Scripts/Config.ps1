c75eb278df29763605712456c948cca458f1c477
tom.gray <tom.gray@tribalgroup.com>
2018-12-04 21:06:13 +1100

    Intial DSC config installs IIS and configures some things

---
+++ Scripts/Config.ps1
@@ -0,0 +1,41 @@
+
+configuration VibServer {
+    param(
+        $computerName = 'localhost',
+        $AppLocation = "C:\app"
+    )
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -Modulename xWebadministration
+
+
+
+
+    Node $computername {
+
+    WindowsFeature Web-Server            { Ensure = 'Present'; Name = 'Web-Server' }
+    WindowsFeature Web-Mgmt-console      { Ensure = 'Present'; Name = 'Web-Mgmt-Console' }
+
+
+    #we'll put our asp.net app here
+    file AppLocation {
+        DestinationPath = $AppLocation
+        Type            = 'Directory'
+        Ensure          = 'Present'
+    }
+
+    #remove the default site so that it returns a non-200 status code.
+    #A theoretical load balancer will not send
+    #any traffic to this host until it is ready.
+    xWebsite RemoveDefaultSite
+    {
+        Ensure          = "Absent"
+        Name            = "Default Web Site"
+        State           = "Stopped"
+    }
+
+
+    
+
+}
+
+}
