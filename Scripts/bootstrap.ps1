e98624c02281b875591a558a8383d9c2a314e002
tom.gray <tom.gray@tribalgroup.com>
2018-12-04 20:47:44 +1100

    add bootstrap script for image: package management tools

---
+++ Scripts/bootstrap.ps1
@@ -0,0 +1,33 @@
+
+#install chocolately package manager
+if (!(get-command choco)) {
+    
+try {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+catch {
+    Write-Error -Message $_
+    throw 10
+}
+
+
+
+}
+
+
+#install nuget package provider (for Powershell module installation)
+if (!(Get-PackageProvider -name nuget)) {
+    try {
+    Install-PackageProvider -Name 'NuGet' -Force -Verbose
+    }
+
+    catch {
+        write-error -Message $_
+        throw 10
+    }
+}
+
+
+#DSC will require these modules:
+install-module xWebAdministration -force -RequiredVersion '2.2.0.0'    
