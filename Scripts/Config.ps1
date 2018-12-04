27128d079bc096e29b8c8576a82dc252311ad1d2
tom.gray <tom.gray@tribalgroup.com>
2018-12-04 21:14:21 +1100

    ensure config is applied when run b the shell provisioner

--- Scripts/Config.ps1
+++ Scripts/Config.ps1
@@ -12,30 +12,34 @@ configuration VibServer {
 
     Node $computername {
 
-    WindowsFeature Web-Server            { Ensure = 'Present'; Name = 'Web-Server' }
-    WindowsFeature Web-Mgmt-console      { Ensure = 'Present'; Name = 'Web-Mgmt-Console' }
+        WindowsFeature Web-Server            { Ensure = 'Present'; Name = 'Web-Server' }
+        WindowsFeature Web-Mgmt-console      { Ensure = 'Present'; Name = 'Web-Mgmt-Console' }
 
 
-    #we'll put our asp.net app here
-    file AppLocation {
-        DestinationPath = $AppLocation
-        Type            = 'Directory'
-        Ensure          = 'Present'
+        #we'll put our asp.net app here
+        file AppLocation {
+            DestinationPath = $AppLocation
+            Type            = 'Directory'
+            Ensure          = 'Present'
+        }
+
+        #remove the default site so that it returns a non-200 status code.
+        #A theoretical load balancer will not send
+        #any traffic to this host until it is ready.
+        xWebsite RemoveDefaultSite
+        {
+            Ensure          = "Absent"
+            Name            = "Default Web Site"
+            State           = "Stopped"
+        }
+
+
+
+
     }
-
-    #remove the default site so that it returns a non-200 status code.
-    #A theoretical load balancer will not send
-    #any traffic to this host until it is ready.
-    xWebsite RemoveDefaultSite
-    {
-        Ensure          = "Absent"
-        Name            = "Default Web Site"
-        State           = "Stopped"
-    }
-
-
-    
 
 }
 
-}
+#generate .mof and appy configuration when this script executed by the vagrant shell provisioner
+VibServer 
+Start-DscConfiguration -path ./Vibserver -wait -verbose
