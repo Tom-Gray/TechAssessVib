5aa9da5a766f1d473c432baf8b1e0c4c6c37f8a4
Tom Gray <tom.gray@tribalgroup.com>
2018-12-06 10:02:22 +1100

    First run at an app that returns content.

---
+++ App/VibApp/responder.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VibApp
+{   
+    public class Responder : Controller
+    {
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Get()
+        {
+                
+                return Content("Replace this message with data from the database");
+            }
+        }
+    }
+
