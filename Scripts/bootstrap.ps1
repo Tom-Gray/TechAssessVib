037c5d19f4405c3cbb3082514717ec79b99a884b
tom.gray <tom.gray@tribalgroup.com>
2018-12-06 12:28:41 +1100

    Committed wrong version of sqlserverdsc module

--- Scripts/bootstrap.ps1
+++ Scripts/bootstrap.ps1
@@ -3,7 +3,7 @@
 #copy modules into the host rather than download them from the web, which is proving unreliable
 
 try {
-Copy-Item "C:\vagrant_data\Modules" -destination "C:\Program Files\WindowsPowershell" -force -recurse -verbose
+Copy-Item "C:\vagrant_data\Modules\" -destination "C:\Program Files\WindowsPowershell" -force -recurse 
 }
 
 catch {
