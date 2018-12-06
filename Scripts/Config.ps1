f0ec346be6c6eef9934a72bef1cdf092761ab005
tom.gray <tom.gray@tribalgroup.com>
2018-12-07 09:31:34 +1100

    Ward off any potential perf issues\reboots

--- Scripts/Config.ps1
+++ Scripts/Config.ps1
@@ -19,6 +19,7 @@ configuration VibServer {
         LocalConfigurationManager
         {
              CertificateId = $node.Thumbprint
+             RebootNodeIfNeeded = $false
         }
 
         WindowsFeature Web-Server            { Ensure = 'Present'; Name = 'Web-Server' }
